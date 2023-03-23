c95cfc689f13aeb29d3e0346d0a18780dd19c58d
Victor Kasap <kasap.victor@gmail.com>
2023-03-23 13:16:29 +0200

    feat: Added Husky

--- vite.config.js
+++ vite.config.js
@@ -1,66 +1,63 @@
 import path from 'node:path';
+
+import autoprefixer from 'autoprefixer';
 import { defineConfig } from 'vite';
-import autoprefixer from 'autoprefixer'
 
-import pages from './pages.config'
+import pages from './pages.config';
 
-const pagesInput = {}
+const pagesInput = {};
 
-pages.forEach((page => {
-    pagesInput[page.name] = path.resolve(__dirname, page.path)
-}));
+pages.forEach((page) => {
+  pagesInput[page.name] = path.resolve(__dirname, page.path);
+});
 
 export default defineConfig(() => {
-	return {
-		root: path.resolve(__dirname, 'src'),
-		base: './',
-		server: {
-			// not used rn, but may be needed in the future
-			port: 8080,
-			host: '0.0.0.0',
-			watch: {
-				usePolling: true,
-			},
-		},
+  return {
+    root: path.resolve(__dirname, 'src'),
+    base: './',
+    server: {
+      // not used rn, but may be needed in the future
+      port: 8080,
+      host: '0.0.0.0',
+      watch: {
+        usePolling: true,
+      },
+    },
     resolve: {
-			alias: [
-				{
-					find: 'root',
-					replacement: path.resolve(__dirname, 'src'),
-				},
-			],
-			extensions: ['.css', '.scss', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-		},
-		build: {
+      alias: [
+        {
+          find: 'root',
+          replacement: path.resolve(__dirname, 'src'),
+        },
+      ],
+      extensions: ['.css', '.scss', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    },
+    build: {
       outDir: path.resolve(__dirname, 'dist'),
       rollupOptions: {
         input: {
-          ...pagesInput
+          ...pagesInput,
         },
         output: {
           assetFileNames: (assetInfo) => {
-            let info = assetInfo.name.split(".");
+            const info = assetInfo.name.split('.');
             let extType = info[info.length - 1];
             if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-              extType = "img";
+              extType = 'img';
             } else if (/woff|woff2/.test(extType)) {
-              extType = "css";
+              extType = 'css';
             }
-              return `assets/${extType}/[name][extname]`;
-            },
-            chunkFileNames: "assets/js/[name].js",
-            entryFileNames: "assets/js/[name].js",
-        }
-      }
+            return `assets/${extType}/[name][extname]`;
+          },
+          chunkFileNames: 'assets/js/[name].js',
+          entryFileNames: 'assets/js/[name].js',
+        },
+      },
     },
-		css: {
+    css: {
       postcss: {
-        plugins: [
-          autoprefixer({})
-        ],
-      }
-    }
-	};
+        plugins: [autoprefixer({})],
+      },
+    },
+  };
 });
-
-
